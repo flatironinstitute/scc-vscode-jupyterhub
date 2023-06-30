2b9a10901b0eebebc9fdecb6d70b577b37d12295
Lehman Garrison <lgarrison@flatironinstitute.org>
2023-06-30 16:20:53 -0400

    Log to $HOME/.vscodehub

--- bin/code-runner.sh
+++ bin/code-runner.sh
@@ -10,11 +10,17 @@ hubdir=$httpdir/user/$USER/
 rm -rf $hubdir
 mkdir -p $hubdir
 
+logdir=$HOME/.vscodehub
+jobid=$(basename $VSCODEHUB_RUNTIME_DIR)
+logfile=$logdir/code-tunnel-$jobid.log
+mkdir -p $logdir
+ln -s $logfile $hubdir/code-tunnel.log
+
 sed "s/HOSTNAME/$(hostname -s)/g" $rootdir/share/index.html > $hubdir/index.html
 
 cp $rootdir/bin/code $hubdir/code
 
-$hubdir/code tunnel --name="rusty" --accept-server-license-terms &> $hubdir/code-tunnel.log &
+$hubdir/code tunnel --name="rusty" --accept-server-license-terms $VSCODE_ARGS &> $logfile &
 
 port="${@#--port=}"
 python -m http.server $port --directory=$httpdir &
