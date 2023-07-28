ece009526c9ea91447ddb0c447577e3ca7d08227
Lehman Garrison <lgarrison@flatironinstitute.org>
2023-07-28 10:17:58 -0400

    Update CLI before launching tunnel

--- bin/code-runner.sh
+++ bin/code-runner.sh
@@ -15,12 +15,14 @@ jobid=$(basename $VSCODEHUB_RUNTIME_DIR)
 logfile=$logdir/code-tunnel-$jobid.log
 mkdir -p $logdir
 ln -s $logfile $hubdir/code-tunnel.log
+: > $logfile
 
 sed "s/HOSTNAME/$(hostname -s)/g" $rootdir/share/index.html > $hubdir/index.html
 
 cp $rootdir/bin/code $hubdir/code
 
-$hubdir/code tunnel --name="rusty" --accept-server-license-terms $VSCODE_ARGS &> $logfile &
+$hubdir/code update $VSCODE_ARGS &>> $logfile || true
+$hubdir/code tunnel --name="rusty" --accept-server-license-terms $VSCODE_ARGS &>> $logfile &
 
 port="${@#--port=}"
 python -m http.server $port --directory=$httpdir &
